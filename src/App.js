b366c75d3c386178a5ae6e59fd87d41d03a40b73
oussema <oussema.kraim@gmail.Com>
2019-11-22 21:45:02 +0100

    clock App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      time: new Date(),
+      displayString: ''
+    }
+  }
+  componentDidMount(){
+  }
+  currentTime(){
+    this.setState(
+      {time : new Date()}
+    )
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  componentWillMount(){
+    console.log("hello")
+    this.interval = setInterval(()=>{
+      if((this.state.time.getSeconds()%5 === 0) && (this.state.time.getSeconds()%3 === 0)){
+        this.setState({
+          displayString: 'FizzBuzz'
+        })
+      }else if(this.state.time.getSeconds()%5 === 0){
+        this.setState({
+          displayString: 'Buzz'
+        })
+      }else if(this.state.time.getSeconds()%3 === 0){
+        this.setState({
+          displayString: 'Fizz'
+        })
+      }else{
+        this.setState({
+          displayString: ''
+        })
+      }
+      return this.currentTime(),1000})
+  }
+  render(){
+
+    return (
+      <div class ="time">
+        <h1> {this.state.time.toLocaleTimeString()} </h1>
+        <h1>{this.state.time.getSeconds()}</h1>
+        <p> Fizz, Buzz or FizzBuzz?? : <strong>{this.state.displayString}</strong> </p>
+      </div>
+    )
+  }
 }
 
 export default App;
